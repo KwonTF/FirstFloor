5f00711886da44b42ac5d0a1f4622f1ce269fa0d
KwonTF <ktf1008@ssu.ac.kr>
2018-04-10 16:04:21 +0900

    Motivity
    motivity start

---
+++ PData/Motivity.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Motivity : MonoBehaviour {
+
+    public AudioClip clip;
+    public VideoClip bga;
+    public VideoClip bga_blur;
+    public Texture normal_img;
+    public Texture hard_img;
+    public Texture extreme_img;
+
+
+    Data.MusicInfo MusicInfo = new Data.MusicInfo();
+    void Start()
+    {
+        MusicInfo.music = clip;
+        MusicInfo.bga_blur = bga_blur;
+        MusicInfo.bga = bga;
+    }
+    public Data.MusicInfo GetInfo()
+    {
+        MusicInfo.key = 1;
+        MusicInfo.name = "MOTIVITY";
+        MusicInfo.composer = "ESTi";
+        MusicInfo.visualizer = "";
+        MusicInfo.music = clip;
+        MusicInfo.bga_blur = bga_blur;
+        MusicInfo.bga = bga;
+        MusicInfo.list = new List<Data.Playdata>();
+        ListInit();
+
+        return MusicInfo;
+    }
+    void ListInit()
+    {
+        int comboCalc = 0;
+        Data.Playdata normal = new Data.Playdata();
+        normal.Difficulty = "Normal";
+        normal.catchParase = normal_img;
+        normal.level = 5;
+        CreateNote(normal);
+        foreach (Data.NoteInfo notes in normal.Notes)
+        {
+            if (notes.isLong)
+            {
+                comboCalc += 2;
+            }
+            else
+            {
+                comboCalc++;
+            }
+        }
+        normal.max_combo = comboCalc;
+        MusicInfo.list.Add(normal);
+
+        comboCalc = 0;
+        Data.Playdata hard = new Data.Playdata();
+        hard.Difficulty = "Hard";
+        hard.level = 14;
+        hard.catchParase = hard_img;
+        CreateNote(hard);
+        foreach (Data.NoteInfo notes in hard.Notes)
+        {
+            if (notes.isLong)
+            {
+                comboCalc += 2;
+            }
+            else
+            {
+                comboCalc++;
+            }
+        }
+        hard.max_combo = comboCalc;
+        MusicInfo.list.Add(hard);
+
+        comboCalc = 0;
+        Data.Playdata extreme = new Data.Playdata();
+        extreme.Difficulty = "Extreme";
+        extreme.level = 20;
+        extreme.catchParase = extreme_img;
+        CreateNote(extreme);
+        foreach (Data.NoteInfo notes in extreme.Notes)
+        {
+            if (notes.isLong)
+            {
+                comboCalc += 2;
+            }
+            else
+            {
+                comboCalc++;
+            }
+        }
+        extreme.max_combo = comboCalc;
+        MusicInfo.list.Add(extreme);
+    }
+    void CreateNote(Data.Playdata input)
+    {
+    }
+    }
